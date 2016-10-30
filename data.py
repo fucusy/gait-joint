78d456611821974e7feb2e96909bde2998fcf987
fucusy <fucus@qq.com>
2016-10-30 11:02:30 +0800

    convert to video

--- data.py
+++ data.py
@@ -4,12 +4,12 @@ import os
 import subprocess
 
 import numpy as np
-from skimage.io import imsave
+from skimage.io import imsave, imread
 import skimage.color as color
 
 import config
 from tools import tool, np_helper
-
+import re
 
 
 def load_test_video():
@@ -58,15 +58,19 @@ def do_box(video_path, back_video_path):
         subprocess.call(["ffmpeg", "-i", back_video_path, output])
 
     # get mean back img
-    back_img_path = tool.load_img_path_list(back_folder)
-    back_img = tool.img_path_2_pic(back_img_path)
-    mean_back_img = np.mean(back_img, axis=0)
-    mean_back_img = mean_back_img.astype(np.int32).astype(np.uint8)
 
     mean_filename = "%s/avg.jpg" % back_folder
-    imsave(mean_filename, mean_back_img)
+    if not os.path.exists(mean_filename):
+        back_img_path = tool.load_img_path_list(back_folder)
+        back_img = tool.img_path_2_pic(back_img_path)
+        mean_back_img = np.mean(back_img, axis=0)
+        mean_back_img = mean_back_img.astype(np.int32).astype(np.uint8)
+        imsave(mean_filename, mean_back_img)
+    else:
+        mean_back_img = imread(mean_filename)
 
-    img_path = tool.load_img_path_list(img_folder)
+
+    img_path = tool.load_img_path_list(img_folder, re.compile(r"\d{4}\.jpg"))
     # do subtraction
     img_data = tool.img_path_2_pic(img_path)
     grey_img_data = np.array([color.rgb2gray(x) for x in img_data])
@@ -78,8 +82,11 @@ def do_box(video_path, back_video_path):
     box_file.write("\n")
 
     for i, img in enumerate(grey_img_data):
-        res = tool.subtract(img, back_grey_img)
         base = os.path.basename(img_path[i])
+
+        save_filename = "%s/%s_cover.bmp" % (img_folder, os.path.splitext(base)[0])
+        res = tool.subtract(img, back_grey_img, save_filename)
+
         line = [base]
         for el in res:
             line.append(str(el))
@@ -101,8 +108,8 @@ def do_joint(crop_folder):
     :param crop_folder:  the folder contain cropped image
     :return:
     """
-
-    return True
+    subprocess.call(["cd", config.project.human_pose_path])
+    subprocess.call(["th", "test.lua", "-useGPU", "1", "-img_folder", crop_folder])
 
 
 def update_joint(joint_folder):
@@ -122,14 +129,56 @@ def recover_video_with_joint(joint_folder):
     :param joint_folder: the folder contain joint data
     :return: video path
     """
-    joint_video = ""
+
+    box_filename = "%s/box_file.txt" % joint_folder
+    joints = []
+    img_filenames = []
+    boxs = []
+
+    is_first = True
+    for line in open(box_filename):
+        if is_first:
+            is_first = False
+            continue
+        split = line.rstrip('\n').split('\t')
+        if len(split) < 5:
+            continue
+        img_filename = "%s/%s" % (joint_folder, split[0])
+        img_base, ext = os.path.splitext(split[0])
+        joint_filename = "%s/%s_extract_joint.txt" % (joint_folder, img_base)
+        joint = []
+        boxs.append([int(x) for x in split[1:]])
+
+        if os.path.exists(joint_filename):
+            for j in open(joint_filename):
+                split = j.rstrip('\n').split('\t')
+                relative_width, relative_height = [int(x) for x in split]
+                joint.append([boxs[-1][1]+relative_width, boxs[-1][0]+relative_height])
+        joints.append(joint)
+        img_filenames.append(img_filename)
+
+    img_data = tool.img_path_2_pic(img_filenames)
+
+    for i in range(len(joints)):
+        img_base, ext = os.path.splitext(os.path.basename(img_filenames[i]))
+        plot_filename = "%s/%s_plot_joint.jpg" % (joint_folder, img_base)
+        plot_img = tool.plot_joint(img_data[i], joints[i])
+        plot_box = tool.plot_box(plot_img, boxs[i])
+        imsave(plot_filename, plot_box)
+
+    joint_video = "%s/plot_joint.avi" % joint_folder
+    img_tpl = "%s/%%04d_plot_joint.jpg" % joint_folder
+    subprocess.call(["ffmpeg", "-i", img_tpl, "-c:v", "libx264", joint_video])
+
     return joint_video
 
 
 def test():
     video_path = config.data.test_video_path
     back_path = config.data.test_back_path
-    do_box(video_path, back_path)
+    img_folder = do_box(video_path, back_path)
+    do_joint(img_folder)
+    recover_video_with_joint(img_folder)
 
 
 if __name__ == '__main__':
--- tools/tool.py
+++ tools/tool.py
@@ -6,7 +6,10 @@ import matplotlib.pyplot as plt
 import skimage.color as color
 from skimage import filter
 from skimage.morphology import disk
+from PIL import Image, ImageDraw
 import config
+import re
+
 
 def extract_info_from_path(path):
     """
@@ -24,15 +27,24 @@ def extract_info_from_path(path):
     return hid, cond, seq, view
 
 
-def load_img_path_list(path):
+def load_img_path_list(path, pattern=None):
     """
 
     :param path: the test img folder
     :return:
     """
     list_path = os.listdir(path)
+    filtered = []
     # change to reg to match extension
-    result = ["%s/%s" % (path, x) for x in list_path if x.endswith("jpg") or x.endswith("png") or x.endswith("bmp")]
+
+    p = re.compile(r".*\.[jpg|png|bmp]")
+    for x in list_path:
+        if re.match(p, x):
+            if pattern is not None and re.match(pattern, x):
+                filtered.append(x)
+            elif pattern is None:
+                filtered.append(x)
+    result = ["%s/%s" % (path, x) for x in filtered]
     return np.array(result)
 
 
@@ -52,8 +64,11 @@ def im_show(im):
     :param im: numpy.array
     :return:
     """
+    plt.clf()
     plt.imshow(im)
-    plt.draw()
+    plt.show()
+    plt.close()
+
 
 
 def get_human_position(img):
@@ -106,11 +121,12 @@ def get_human_position(img):
     return [left_up_corner_i, left_up_corner_j, box_i, box_j]
 
 
-def subtract(img, back):
+def subtract(img, back, save_filename):
     """
 
     :param img: numpy array
     :param back: numpy array
+    :param save_filename: save the sub result to this filename
     :return: a black and white img stored in numpy array
     """
 
@@ -131,17 +147,86 @@ def subtract(img, back):
     sub_img_uint8 = sub_img_uint8.astype(np.uint8)
     sub_img_uint8 = filter.median(sub_img_uint8, disk(5))
 
+    sub_img = sub_img_uint8.astype(np.float32) / 255
 
-    # sub_img = sub_img_uint8.astype(np.float32) / 255
-    # count = 0
-    # filename = "%s/test_img/sub_img%5d.bmp" % (config.project.data_path, count)
-    # while os.path.exists(filename):
-    #     count += 1
-    #     filename = "%s/test_img/sub_img%05d.bmp" % (config.project.data_path, count)
-    #
-    # dirname = os.path.dirname(filename)
-    # if not os.path.exists(dirname):
-    #     os.makedirs(dirname)
-    # imsave(filename, sub_img)
+    dirname = os.path.dirname(save_filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    imsave(save_filename, sub_img)
 
     return get_human_position(sub_img_uint8)
+
+
+def plot_joint(img, joints):
+    """
+    :param img:
+    :param joints: joint id (0 - r ankle, 1 - r knee, 2 - r hip, 3 - l hip, 4 - l knee,
+                    5 - l ankle, 6 - pelvis, 7 - thorax, 8 - upper neck, 9 - head top,
+                    10 - r wrist, 11 - r elbow, 12 - r shoulder, 13 - l shoulder,
+                    14 - l elbow, 15 - l wrist)
+    :return: the img after put skeleton on it
+    """
+
+    pairs = np.array(
+        [[1, 2], [2, 3], [3, 7], [4, 5], [4, 7], [5, 6], [7, 9], [9, 10], [14, 9], [11, 12], [12, 13], [13, 9],
+         [14, 15], [15, 16]]) - 1
+
+
+
+    img = Image.fromarray(img.astype(np.uint8))
+    draw = ImageDraw.Draw(img)
+
+    for i in range(pairs.shape[0]):
+
+        if len(joints) > pairs[i, 0] and len(joints) > pairs[i, 1]:
+            # plot only the visible joints
+            a_x, a_y = joints[pairs[i, 0]]
+            b_x, b_y = joints[pairs[i, 1]]
+            draw.line((a_x, a_y, b_x, b_y), width=3, fill=(255, 0, 0))
+    return np.asarray(img)
+
+def plot_box(img, box):
+    """
+
+    :param img:
+    :param box:left up corner and width, height of the box,
+        [{left-up-corner-i}, {left-up-corner-j}, {box-i}, {box-j}]
+
+        you can get the left up pixel by img[{left-up-corner-i}][{left-up-corner-j}]
+        right bottom pixel by img[{left-up-corner-i}+{box-i}][{left-up-corner-j}+{box-j}]
+    :return:
+    """
+    img = Image.fromarray(img.astype(np.uint8))
+    draw = ImageDraw.Draw(img)
+
+    draw.line((box[1], box[0], box[1] + box[3], box[0]))
+
+    draw.line((box[1], box[0]+box[2], box[1] + box[3], box[0] + box[2]))
+
+    draw.line((box[1], box[0], box[1], box[0] + box[2]))
+
+    draw.line((box[1] + box[3], box[0], box[1] + box[3], box[0] + box[2]))
+
+    return np.asarray(img)
+
+
+def read_list_from(filename):
+    joint = []
+    for j in open(filename):
+        relative_x, relative_y = map(int, j.rstrip('\n').split('\t'))
+        joint.append([relative_x, relative_y])
+    return joint
+
+if __name__ == '__main__':
+    filename = "/Users/fucus/Documents/irip/gait_recoginition/code/gait-joint/data/001/001-nm-01-090/0069_extract.jpg"
+    img = imread(filename)
+    joint_filename = "/Users/fucus/Documents/irip/gait_recoginition/code/gait-joint/data/001/001-nm-01-090/0069_extract_joint.txt"
+    joints = read_list_from(joint_filename)
+    plot_img = plot_joint(img, joints)
+    im_show(plot_img)
+
+    filename = "/Users/fucus/Documents/irip/gait_recoginition/code/gait-joint/data/001/001-nm-01-090/0069.jpg"
+    box = [48, 165, 147, 35]
+    img = imread(filename)
+    box_img = plot_box(img, box)
+    im_show(box_img)
